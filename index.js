ba666201110da68d97db8f754afdaa6025839c9d
Chris Cardle <cdcardle@gmail.com>
2018-10-08 17:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function exactMatch(drivers, obj) {
   const key = Object.keys(obj);
   return drivers.filter((driver) => driver[key] === obj[key]);
 }
+
+function exactMatchToList(drivers, obj) {
+  return exactMatch(drivers, obj).map(driver => driver.name)
+}
