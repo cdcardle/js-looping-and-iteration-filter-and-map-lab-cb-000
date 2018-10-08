f1e91c9307a032f6f5675ea08bbf9fe7fdbb8268
Chris Cardle <cdcardle@gmail.com>
2018-10-08 16:33:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter((driver) => driver.revenue > revenue.strip(0))
+  return drivers.filter((driver) => driver.revenue >= revenue.strip(0))
 }
